82e4ba63d5b737af28bc204220352af8367f5325
JazminMendoza <jazminrominamm@gmail.com>
2024-07-18 09:55:18 -0300

    primera parte

---
+++ src/middlewares/middlewares.js
@@ -0,0 +1,23 @@
+import DTOuser from "../DTO/user.dto.js"
+
+export const rolAuthenticationAdmin = async(req, res, next) => {
+    if(!req.session.login){
+        return res.redirect("/login")
+    }
+    const DTOusers = new DTOuser(req.session.user)
+    if(DTOusers.rol == "Admin"){
+        return next()
+    }
+    return res.redirect("profile")
+}
+
+export const rolAuthenticationUser = async(req, res, next) => {
+    if(!req.session.login){
+        return res.redirect("/login")
+    }
+    const DTOusers = new DTOuser(req.session.user)
+    if(DTOusers.rol == "User"){
+        return next()
+    }
+    return res.redirect("profile")
+}
